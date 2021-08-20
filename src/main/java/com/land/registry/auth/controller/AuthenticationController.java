b0132e887fd3bcc564636f5ea233ff026c1e2fb2
Kieran McGowan <kmcgowan@mediciland.com>
2021-08-20 13:39:28 +0100

    some sec checks

--- src/main/java/com/land/registry/auth/controller/AuthenticationController.java
+++ src/main/java/com/land/registry/auth/controller/AuthenticationController.java
@@ -4,14 +4,24 @@ import com.land.registry.auth.model.dto.ApiResponse;
 import com.land.registry.auth.model.dto.JwtAuthenticationResponse;
 import com.land.registry.auth.model.dto.LoginRequest;
 import com.land.registry.auth.model.dto.SignUpRequest;
+import com.land.registry.auth.model.dto.UserIdentityAvailability;
+import com.land.registry.auth.model.dto.UserProfile;
+import com.land.registry.auth.model.dto.UserSummary;
+import com.land.registry.auth.model.entity.User;
 import com.land.registry.auth.service.AuthService;
+import com.land.registry.auth.util.security.CurrentUser;
+import com.land.registry.auth.util.security.UserPrincipal;
 import java.net.URI;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +33,31 @@ public class AuthenticationController {
   @Inject
   public AuthenticationController(final AuthService authService) {
     this.authService = authService;
+  }
+
+  @GetMapping("/user/me")
+  @PreAuthorize("hasRole('USER')")
+  public UserSummary getCurrentUser(@CurrentUser final UserPrincipal currentUser) {
+    return new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+  }
+
+
+  @GetMapping("/user/checkUsernameAvailability")
+  public UserIdentityAvailability checkUsernameAvailability(
+      @RequestParam(value = "username") final String username) {
+    return new UserIdentityAvailability(authService.isUsernameAvailable(username));
+  }
+
+  @GetMapping("/user/checkEmailAvailability")
+  public UserIdentityAvailability checkEmailAvailability(
+      @RequestParam(value = "email") final String email) {
+    return new UserIdentityAvailability(authService.isEmailAvailable(email));
+  }
+
+  @GetMapping("/users/{username}")
+  public UserProfile getUserProfile(@PathVariable(value = "username") final String username) {
+    final User user = authService.findUserOrThrow(username);
+    return new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getEmail());
   }
 
   @PostMapping("/auth/signup")
--- src/main/java/com/land/registry/auth/service/AuthService.java
+++ src/main/java/com/land/registry/auth/service/AuthService.java
@@ -1,12 +1,19 @@
 package com.land.registry.auth.service;
 
-import com.land.registry.auth.model.dto.LoginRequest;
-import com.land.registry.auth.model.dto.SignUpRequest;
-import com.land.registry.auth.model.entity.User;
+import static com.land.registry.auth.model.enums.RoleName.ROLE_USER;
+
+import com.google.common.base.Preconditions;
 import com.land.registry.auth.repository.RoleRepository;
 import com.land.registry.auth.repository.UserRepository;
+import com.land.registry.auth.exception.AppException;
+import com.land.registry.auth.exception.ResourceNotFoundException;
+import com.land.registry.auth.model.dto.LoginRequest;
+import com.land.registry.auth.model.dto.SignUpRequest;
+import com.land.registry.auth.model.entity.Role;
+import com.land.registry.auth.model.entity.User;
 import com.land.registry.auth.util.security.JwtTokenProvider;
 import java.net.URI;
+import java.util.Collections;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.validation.Valid;
@@ -25,6 +32,8 @@ public class AuthService {
   private static final Logger LOGGER = Logger.getLogger(AuthService.class);
 
   private final UserRepository userRepository;
+  private final RoleRepository roleRepository;
+  private final PasswordEncoder passwordEncoder;
   private final AuthenticationManager authenticationManager;
   private final JwtTokenProvider tokenProvider;
 
@@ -33,6 +42,8 @@ public class AuthService {
       final PasswordEncoder passwordEncoder,
       final AuthenticationManager authenticationManager, final JwtTokenProvider tokenProvider) {
     this.userRepository = userRepository;
+    this.roleRepository = roleRepository;
+    this.passwordEncoder = passwordEncoder;
     this.authenticationManager = authenticationManager;
     this.tokenProvider = tokenProvider;
   }
@@ -41,11 +52,47 @@ public class AuthService {
     SecurityContextHolder.getContext().setAuthentication(authentication);
   }
 
+  public Boolean isUsernameAvailable(final String username) {
+    return !userRepository.existsByUsername(username);
+  }
+
+  public Boolean isEmailAvailable(final String email) {
+    return !userRepository.existsByEmail(email);
+  }
+
+  public User findUserOrThrow(final String username) {
+    return userRepository.findByUsername(username)
+        .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+  }
+
   public URI registerUser(final SignUpRequest signUpRequest) {
+    verifyDto(signUpRequest);
+
     final User user = getUser(signUpRequest);
+    setRole(user);
     userRepository.save(user);
 
     return buildURI(signUpRequest.getUsername());
+  }
+
+  private void verifyDto(final SignUpRequest signUpRequest) {
+    Preconditions.checkArgument(signUpRequest.getUsername() != null, "Username can't be null");
+    Preconditions.checkArgument(signUpRequest.getEmail() != null, "Email can't be null");
+    Preconditions.checkArgument(signUpRequest.getName() != null, "Name can't be null");
+    Preconditions.checkArgument(signUpRequest.getPassword() != null, "Password can't be null");
+
+    Preconditions.checkState(!userRepository.existsByUsername(signUpRequest.getUsername()),
+        "Username is already taken. Please try another");
+
+    Preconditions.checkState(!userRepository.existsByEmail(signUpRequest.getEmail()),
+        "An account is already associated with this email");
+  }
+
+  private void setRole(final User user) {
+    final Role userRole = roleRepository.findByName(ROLE_USER)
+        .orElseThrow(() -> new AppException("User Role not set."));
+
+    user.setRoles(Collections.singleton(userRole));
   }
 
   private URI buildURI(final String username) {
@@ -56,11 +103,24 @@ public class AuthService {
   }
 
   User getUser(@Valid @RequestBody final SignUpRequest signUpRequest) {
+    final String encPw = passwordEncoder.encode(signUpRequest.getPassword());
+
     return new User(signUpRequest.getName(), signUpRequest.getUsername(), signUpRequest.getEmail(),
-        signUpRequest.getPassword());
+        encPw);
   }
 
   public String authenticateUser(final LoginRequest loginRequest) {
+    Preconditions
+        .checkArgument(loginRequest.getUsernameOrEmail() != null, "Must Provide Email or Username");
+
+    Preconditions.checkArgument(loginRequest.getPassword() != null, "Must Provide a Password");
+
+    LOGGER.info("Username or Email: " + loginRequest.getUsernameOrEmail());
+
+    LOGGER.info("Username/Email Exists? " + userRepository
+        .existsByUsernameOrEmail(loginRequest.getUsernameOrEmail(),
+            loginRequest.getUsernameOrEmail()));
+
     final Authentication authentication = authenticationManager.authenticate(
         new UsernamePasswordAuthenticationToken(loginRequest.getUsernameOrEmail(),
             loginRequest.getPassword()));
